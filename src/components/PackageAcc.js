7a8fe8266b19b8f735e649fe43170f002012d233
Mohd Amirulhadi <mohdamirulhadimohdn@airasia.com>
2022-08-12 07:23:03 +0800

    Fixing Tab in Product Services and form in Career page

--- src/components/PackageAcc.js
+++ src/components/PackageAcc.js
@@ -1,11 +1,115 @@
 import React from 'react';
+import {CheckIcon, InformationCircleIcon} from '@heroicons/react/solid'
 
-function PackageAcc (){
+export default function Package () {
     return (
-        <div>
-            <p>This is package 1</p>
+        <>
+        <div className='grid grid-cols-3 gap-4 w-auto h-auto'>
+            <div className='w-30 h-auto rounded-md bg-slate-100'>
+                <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>Monthly Accounting Fees</p>
+                    <p className='p-5 pb-0 font-bold'> For a sales turnover of RM 400,000.00 and above</p>
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1'>Range from RM 500.00 – RM 1,000.00 monthly</p>
+                    </div>
+
+                    {/* Remarks */}
+                    <p className='p-5 pb-0 italic'>Remarks:</p>
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Pricing of monthly accounts will be different with referring to the amount of our work.</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Accounting Checklist for Sdn. Bhd./ Berhad.pdf</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Accounting Checklist for Sole Prop. / Partnership/ LLP/ PLT.pdf</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>We will quote individually for sales turnover more than RM 1 million.</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Fast Track accounting services will be adding on additional RM 318.00 (inclusive SST).</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Claiming from Un-claim money from Jabatan Akauntan Negara at RM 300.00 which no inclusive of out of pocket reimbursements and SST</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <InformationCircleIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1 pb-5'>Preparation of Capital Statements from RM 300.00 in consideration the level of complexity</p>
+                    </div>
+
+            </div>
+            <div className='w-30 h-80 rounded-md bg-slate-100'>
+            <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>Yearly Accounting Fees</p>
+                <p className='p-5 pb-0 font-bold'>For a sales turnover per annum from RM 50,000.00 to RM 399,999.99.</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>Range from RM 800.00 (dormant) – RM 2,800.00 per annum </p>
+                </div>
+            </div>
+            <div className='w-30 h-auto rounded-md bg-slate-100 pb-10'>
+            <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>A La Carte Accounting Fees</p>
+                <p className='p-5 pb-0 font-bold'>Cash Flows Projections</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-20'>Range from RM 300.00 per page</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Financial Presentations</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>With related stakeholders or shareholders @ RM 250.00 per hour</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Representing client dealing with IRB</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 250.00 per hour</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Finalization & Presentation (Forensic) of Accounts</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>Professional fees from RM 1,000.00 (depends on the complexity of accounts)</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Documents Housekeeping</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>From RM 100.00- RM 300.00 as it depends to quantity of house-keep</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Site visit with consultation</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 250.00 per hour</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Reverse pick-up</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>Docs to be collected by our appointed courier team starts from RM 15.00 (depends on weight)</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>HC Tax Submission Task List</p>
+                <div className='flex flex-row items-center justify-between '>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>HC Tax Submission Task List.form</p>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
-
-export default PackageAcc;
--- src/components/PackageLLP.js
+++ src/components/PackageLLP.js
@@ -1,11 +1,207 @@
 import React from 'react';
+import {CheckIcon, InformationCircleIcon} from '@heroicons/react/solid'
 
-function PackageLLP () {
+export default function PackageLLP () {
     return (
-        <div>
-            <p>This is package 2</p>
+        <>
+        <div className='grid grid-cols-3 gap-4 w-auto h-auto'>
+            <div className='w-30 h-auto rounded-md bg-slate-100'>
+                <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>LLP / PLT Services and Fees</p>
+                    <p className='p-5 pb-0 font-bold'>Incorporation of LLP</p>
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='pr-5 pl-5 flex flex-1'>RM 1,664.80</p>
+                    </div>
+                    <p className='italic pl-5'>Inclusive of Company Profile, LLP Partner Agreement, Company Chop, Opening of UOB Current Bank Account & LLP Incorporation Fees.</p>
+            </div>
+
+            <div className='w-30 h-auto rounded-md bg-slate-100'>
+                <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>LLP Services Yearly Package</p>
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Annual declaration submission to SSM worth RM 300.00</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Up to date LLP Registers and Statutory Record</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Allocation of registered office address**</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>General consultation on limited liability partnerships/compliance/accounting/tax matters</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Oversee and ensuring compliance with relevant legal requirements, particularly under the Limited Liability Partnerships Act 2012 and Income Tax Act 1967</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Regular compliance alert via email and call</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Compliance Officer’s reading on compliance requirements</p>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-between'>
+                    <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                    <p className='p-5 flex flex-1'>Use of HC’s Ipoh Address as your LLP registered office to comply with the statutory requirement under the Limited Liability Partnership Act 2012.</p>
+                    </div>
+            </div>
+
+
+
+
+            <div className='w-30 h-auto pb-20 rounded-md bg-slate-100'>
+            <p className='p-5 text-center bg-blue-500 w-auto h-auto rounded-md rounded-b-none text-white uppercase'>Àla Carte LLP/PLT Services</p>
+                <p className='p-5 pb-0 font-bold'>LLP Yearly Package</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 800.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>LLP Name Search</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 50.00 (each application)</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of LLP Name</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 100.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>LLP Current Profile from SSM</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 300.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Standard LLP Partner Agreement</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>From RM 300.00 (excluding stamp duty)</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Submission of Annual Declaration</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 300.00/annum</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Extension of time for Annual Submission (up to 180 days extension from due date)</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Registered Office Address</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Business Address</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Nature of Business</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Partner Particulars</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00 per partner</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Compliance Officer</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 150.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Appointment / Resignation of Partner(s)</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 100.00 per partner</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Change of Bank Authorised Signatory / Signatories</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 30.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Opening / Closure of Bank Account</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 30.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Certified Statutory Documents by SSM </p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 10.00/ page</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Certified of Documents by Commissioner for Oaths</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 10.00/ page</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Bank Referral Fee</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>F.O.C</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>For a sales turnover per annum from RM 50,000.00 to RM 399,999.99.</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>Range from RM 800.00 (dormant) – RM 2,800.00 per annum </p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Courier Charges</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>From RM 30.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Registered office services</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 40.00 monthly</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>LLP Strike Off/ Winding Up </p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>from RM 1,000.00</p>
+                </div>
+
+                <p className='p-5 pb-0 font-bold'>Register of a trademark</p>
+                <div className='flex flex-row items-center justify-between'>
+                <CheckIcon className="ml-5 h-5 w-5 text-blue-500"/>
+                <p className='pr-5 pl-5 flex flex-1'>RM 2,090.00/ per trademark</p>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
-
-export default PackageLLP;
